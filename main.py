99430c068facf8e70bee573f1b2eda79953545cd
Amirali Sadeghi <a.sadeghifarshi@gmail.com>
2021-03-19 23:17:14 +0330

    Done!

--- main.py
+++ main.py
@@ -1,53 +1,66 @@
 import matplotlib.pyplot as plt
 import random as rand
+from math import sin, cos, pi
 
 
-numPoints = 100
+numPoints = 2000
+shapes = ['random', 'gaussian', 'circle']
+for shape in shapes:
+    if shape == 'random':
+        pointsX = [rand.random() + 1 for ix in range(numPoints)]
+        pointsY = [rand.random() + 2.5 for iy in range(numPoints)]
+    elif shape == 'gaussian':
+        pointsX = [rand.gauss(0, 0.5) for ix in range(numPoints)]
+        pointsY = [rand.gauss(0, 0.5) for ix in range(numPoints)]
+    elif shape == 'circle':
+        tempPointsX = [rand.random() for ix in range(numPoints)]
+        tempPointsY = [rand.random() for iy in range(numPoints)]
+        pointsX = [x * cos(2 * pi * y) for x, y in zip(tempPointsX, tempPointsY)]
+        pointsY = [x * sin(2 * pi * y) for x, y in zip(tempPointsX, tempPointsY)]
 
-pointsX = [rand.random() for ix in range(numPoints)]
-pointsY = [rand.random() for iy in range(numPoints)]
+    bestMinX = min(pointsX[0], pointsX[1])
+    bestMaxX = max(pointsX[0], pointsX[1])
+    bestMinY = min(pointsY[0], pointsY[1])
+    bestMaxY = max(pointsY[0], pointsY[1])
 
-# points = [(1, 2), (1, 3), (2, 0.5)]
-#
-# pointsX = [point[0] for point in points]
-# pointsY = [point[1] for point in points]
+    length = len(pointsX)
+    numPairs = length // 2
 
-bestMinX = min(pointsX[0], pointsX[1])
-bestMaxX = max(pointsX[0], pointsX[1])
-bestMinY = min(pointsY[0], pointsY[1])
-bestMaxY = max(pointsY[0], pointsY[1])
+    for pairIndex in range(1, numPairs):
+        curMinX = min(pointsX[2 * pairIndex], pointsX[2 * pairIndex + 1])
+        curMaxX = max(pointsX[2 * pairIndex], pointsX[2 * pairIndex + 1])
+        curMinY = min(pointsY[2 * pairIndex], pointsY[2 * pairIndex + 1])
+        curMaxY = max(pointsY[2 * pairIndex], pointsY[2 * pairIndex + 1])
 
-length = len(pointsX)
-numPairs = length // 2
+        if curMinX < bestMinX:
+            bestMinX = curMinX
+        if curMaxX > bestMaxX:
+            bestMaxX = curMaxX
+        if curMinY < bestMinY:
+            bestMinY = curMinY
+        if curMaxY > bestMaxY:
+            bestMaxY = curMaxY
 
-for pairIndex in range(1, numPairs):
-    curMinX = min(pointsX[2 * pairIndex], pointsX[2 * pairIndex + 1])
-    curMaxX = max(pointsX[2 * pairIndex], pointsX[2 * pairIndex + 1])
-    curMinY = min(pointsY[2 * pairIndex], pointsY[2 * pairIndex + 1])
-    curMaxY = max(pointsY[2 * pairIndex], pointsY[2 * pairIndex + 1])
+    if length % 2 == 1:
+        if pointsX[-1] > bestMaxX:
+            bestMaxX = pointsX[-1]
+        elif pointsX[-1] < bestMinX:
+            bestMinX = pointsX[-1]
+        if pointsY[-1] > bestMaxY:
+            bestMaxY = pointsY[-1]
+        elif pointsY[-1] < bestMinY:
+            bestMinY = pointsY[-1]
 
-    if curMinX < bestMinX:
-        bestMinX = curMinX
-    if curMaxX > bestMaxX:
-        bestMaxX = curMaxX
-    if curMinY < bestMinY:
-        bestMinY = curMinY
-    if curMaxY > bestMaxY:
-        bestMaxY = curMaxY
-
-if length % 2 == 1:
-    if pointsX[-1] > bestMaxX:
-        bestMaxX = pointsX[-1]
-    elif pointsX[-1] < bestMinX:
-        bestMinX = pointsX[-1]
-    if pointsY[-1] > bestMaxY:
-        bestMaxY = pointsY[-1]
-    elif pointsY[-1] < bestMinY:
-        bestMinY = pointsY[-1]
-
-plt.scatter(pointsX, pointsY)
-plt.hlines(bestMaxY, bestMinX, bestMaxX, 'r')
-plt.hlines(bestMinY, bestMinX, bestMaxX, 'r')
-plt.vlines(bestMinX, bestMinY, bestMaxY, 'r')
-plt.vlines(bestMaxX, bestMinY, bestMaxY, 'r')
-plt.show()
+    plt.scatter(pointsX, pointsY)
+    plt.hlines(bestMaxY, bestMinX, bestMaxX, 'r')
+    plt.hlines(bestMinY, bestMinX, bestMaxX, 'r')
+    plt.vlines(bestMinX, bestMinY, bestMaxY, 'r')
+    plt.vlines(bestMaxX, bestMinY, bestMaxY, 'r')
+    if shape == 'random':
+        plt.title('Random points in a rectangle')
+    elif shape == 'gaussian':
+        plt.title('Gaussian random points with mu=0, sig=0.5')
+    elif shape == 'circle':
+        plt.title('Random points inside a circle')
+    plt.show()
+    plt.savefig(shape)
